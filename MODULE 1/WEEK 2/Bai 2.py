a85402461c0c7cc386bf80279df75cc028fcba86
Admin <danhviet96@gmail.com>
2024-06-17 23:14:21 +0700

    Merge remote-tracking branch 'origin/master'

--- MODULE 1/WEEK 2/Bai 2.py
+++ MODULE 1/WEEK 2/Bai 2.py
@@ -1,17 +1,14 @@
 def count_letters(word):
-    text_count = {}  # Đổi tên biến count thành text_count hoặc tên khác phù hợp
+    text_count = {}
     word = word.lower()
-    for char in word:  # Sử dụng char để duyệt từng ký tự trong từ
-        if char.isalpha():  # Kiểm tra xem char có phải là chữ cái không
+    for char in word:
+        if char.isalpha():
             if char in text_count:
                 text_count[char] += 1
             else:
                 text_count[char] = 1
 
     return text_count
-
-
-# Ví dụ sử dụng
-word = "data"
+word = "Hello"
 result = count_letters(word)
 print(result)
--- MODULE 1/WEEK 2/Bài 3.py
+++ MODULE 1/WEEK 2/Bài 3.py
@@ -1,19 +1,15 @@
 def count_word(path):
-    count = {}  # Khởi tạo một từ điển để lưu trữ số lần xuất hiện của từng từ
+    count = {}
     with open(path, 'r', encoding='utf-8') as file:
-        text = file.read()  # Đọc toàn bộ nội dung của file vào biến text
-        words = text.split()  # Tách các từ trong nội dung thành một danh sách các từ
-
-    # Đếm số lần xuất hiện của mỗi từ trong danh sách các từ
+        text = file.read()
+        words = text.split()
     for word in words:
         word = word.lower()
         if word in count:
             count[word] += 1
         else:
             count[word] = 1
-
     return count
-
 
 path = r'C:\Users\Admin\Desktop\viet\P1_data.txt'
 result = count_word(path)
